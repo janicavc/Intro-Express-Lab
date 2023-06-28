b49084559cd440a6dc309f8f61feba9abd0e45e7
janicavc <crisostomo_janica@yahoo.com>
2023-06-28 00:59:51 -0700

    added games endpoint

---
+++ data/games-db.js
@@ -0,0 +1,13 @@
+const games = [
+    {recommend: 'Valorant', fun: true},
+    {recommend: 'Animal Crossing', fun: true},
+    {recommend: 'League of Legends', fun: false},
+    {recommend: 'It Takes Two', fun: true}
+    {recommend: 'Overwatch 2', fun: false}
+]
+
+module.exports = {
+    getAll: function() {
+        return games;
+    }
+}
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+
+const path = require('path')
+// require game database
+const gamesDb = require('./data/games-db');
 const app = express();
+
+// set
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', function (req, res) {
     res.send('<h1>Welcome to the Home Page!</h1>')
 });
 
+
+app.get('/games', function (req, res) {
+    res.render('games/index', {
+        games: gamesDb.getAll()
+    });
+});
+
+// listen
 app.listen(3001, function () {
     console.log('Listening on port 3001');
 });
+
